d49f45c61270fc5a059c0cef18c191fd957350c5
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-05-29 07:56:26 +0330

    github oauth provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,13 @@
 import NextAuth from 'next-auth/next';
+import GitHubProvider from 'next-auth/providers/github';
 
 const authOptions = {
-  providers: [],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
 };
 
 export default NextAuth(authOptions);
