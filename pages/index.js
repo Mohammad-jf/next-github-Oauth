05bfbb4364e3b8f3ffe9c3c737a9f009f6a6a3f7
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-05-30 07:32:05 +0330

    some fixes

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,30 @@
-import { signIn, signOut } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 
 export default function Home() {
+  const { status } = useSession();
   const signInHandler = async () => {
     signIn('github');
-  }
+  };
   const signOutHandler = async () => {
     signOut();
-  }
+  };
   return (
     <>
       <h1>Next-auth | GitHub</h1>
-      <button onClick={signInHandler}>Sign in</button>
-      <button onClick={signOutHandler}>Sign out</button>
-      <Link href='/dashboard'>
-        <button>Dashboard</button>
-      </Link>
+      
+      {status !== 'authenticated' ? (
+        <button onClick={signInHandler}>Sign in</button>
+      ) : null}
+
+      {status === 'authenticated' ? (
+        <>
+          <button onClick={signOutHandler}>Sign out</button>
+          <Link href='/dashboard'>
+            <button>Dashboard</button>
+          </Link>
+        </>
+      ) : null}
     </>
   );
 }
